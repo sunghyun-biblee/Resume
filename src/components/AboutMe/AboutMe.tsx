81854e4163d6d853f13f4a431efb9c28cb9d658e
sunghyun1014 <sunghyun1014@naver.com>
2024-07-02 21:50:24 +0900

    추가: Introduce,About Me . 자기소개 추가

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,5 +1,78 @@
-import React from "react";
+import { Article, Title } from "App";
+import React, { useRef } from "react";
+import styled from "styled-components";
 
 export const AboutMe = () => {
-  return <div>AboutMe</div>;
+  const handleCopy = (text: string) => {
+    try {
+      navigator.clipboard.writeText(text);
+      alert("클릭보드에 복사되었습니다");
+    } catch (error) {
+      alert("복사에 실패하였습니다");
+    }
+  };
+  return (
+    <Article>
+      <Title className=" after:content-['.'] after:text-blue-400 after:px-1">
+        Contact
+      </Title>
+      <Contact className="flex flex-col justify-end gap-[20px] w-[50%]">
+        <ContactItem>
+          <span className="font-semibold  w-[70px] inline-block mr-[0.5rem]">
+            Email.
+          </span>
+          <button onClick={() => handleCopy("sunghyun543@gmail.com")}>
+            sunghyun543@gmail.com
+          </button>
+          <span className="text-sm text-gray-400"> (메일클릭시 복사)</span>
+        </ContactItem>
+        <ContactItem>
+          <span className="font-semibold w-[70px] inline-block mr-[0.5rem]">
+            Github.
+          </span>
+          <a href="https://github.com/sunghyun-biblee">
+            https://github.com/sunghyun-biblee
+          </a>
+        </ContactItem>
+        <ContactItem>
+          <span className="font-semibold w-[70px] inline-block mr-[0.5rem]">
+            Portfolio.
+          </span>
+          <a href="#">Portfolio</a>
+        </ContactItem>
+        <ContactItem>
+          <span className="font-semibold w-[70px] inline-block mr-[0.5rem]">
+            Phone.
+          </span>
+          <a href="#" className="tracking-wide">
+            010-0000-0000
+          </a>
+        </ContactItem>
+      </Contact>
+    </Article>
+  );
 };
+const Contact = styled.ul`
+  display: flex;
+  justify-content: end;
+  gap: 20px;
+`;
+const ContactItem = styled.li`
+  transition: all 0.3s;
+
+  button {
+    margin-right: 5px;
+  }
+  a,
+  button {
+    color: rgb(107, 114, 128);
+
+    padding-bottom: 1px;
+    border-bottom: 1px solid rgba(107, 114, 128, 0.7);
+    &:hover {
+      border-bottom: 1px solid rgb(59, 130, 246);
+      cursor: pointer;
+      color: rgb(59, 130, 246);
+    }
+  }
+`;
--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -1,27 +1,28 @@
+import { Article, Title } from "App";
 import React from "react";
 import styled from "styled-components";
 
 export const Introduce = () => {
   return (
-    <article>
-      <h1 className="mt-[5rem] mb-[2.3rem]   text-4xl font-bold">
-        <Span className="text-gray-600 py-1">안녕하세요</Span>
+    <article className="mt-[4rem]">
+      <Title className=" after:content-['.'] after:text-blue-400">
+        <Span className="text-gray-600 py-1 inline-block">안녕하세요</Span>
         <br />
         꾸준히 우상향할 개발자 조성현
-        <Span className="text-gray-600 px-1"> 입니다.</Span>
-      </h1>
+        <Span className="text-gray-600 px-1 inline-block">입니다</Span>
+      </Title>
       <p className="leading-7 tracking-tight text-base py-2 border-t-2 border-blue-400/70">
         개발자로 성장하는 과정에서 처음 마주하는 기술들에 시간이 걸리더라도
         외우려 하지 않고 최대한 이해하여 사용하는 것을 선호 합니다.
         <span className="py-1 inline-block">
           프로젝트 진행 중 발생하는 문제에 대해 회피하지 않고 팀원들과 소통하여
-          해결하며, 일정과 리소스를 고려하여 우선순위를 유연하게 조정하여,
+          해결하며, 일정과 리소스를 고려하여 우선순위를 유연하게 조정하고,
           팀원들의 공감을 바탕으로 프로젝트를 끝까지 마무리합니다.
         </span>
         이런 경험을 바탕으로 배움에 대한 열정을 잃지 않고 꾸준한 노력을 기반으로
         멀티캠퍼스 웹 개발자 수료과정에서 개인 최우수상 을 수상했습니다
       </p>
-      <Contact className="flex justify-end gap-[20px]">
+      {/* <Contact className="flex justify-end gap-[20px]">
         <ContactItem>
           <a href="#">sunghyun543@gmail.com</a>
         </ContactItem>
@@ -31,7 +32,7 @@ export const Introduce = () => {
         <ContactItem>
           <a href="#">Portfolio</a>
         </ContactItem>
-      </Contact>
+      </Contact> */}
     </article>
   );
 };
