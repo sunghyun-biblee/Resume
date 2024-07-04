dc449da13141bb6d9c14f84e4ed98fd659be0d3f
sunghyun1014 <sunghyun1014@naver.com>
2024-07-04 19:59:29 +0900

    수정: Title styled-component 적용

--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -5,11 +5,11 @@ import styled from "styled-components";
 export const Introduce = () => {
   return (
     <article className="mt-[4rem]">
-      <Title className=" after:content-['.'] after:text-blue-400">
+      <Title>
         <Span className="text-gray-600 py-1 inline-block">안녕하세요</Span>
         <br />
         꾸준히 우상향할 개발자 조성현
-        <Span className="text-gray-600 px-1 inline-block">입니다</Span>
+        <Span className="text-gray-600 pl-1 inline-block">입니다</Span>
       </Title>
       <p className="leading-7 tracking-tight text-base py-2 border-t-2 border-blue-400/70">
         개발자로 성장하는 과정에서 처음 마주하는 기술들에 시간이 걸리더라도
@@ -22,40 +22,10 @@ export const Introduce = () => {
         이런 경험을 바탕으로 배움에 대한 열정을 잃지 않고 꾸준한 노력을 기반으로
         멀티캠퍼스 웹 개발자 수료과정에서 개인 최우수상 을 수상했습니다
       </p>
-      {/* <Contact className="flex justify-end gap-[20px]">
-        <ContactItem>
-          <a href="#">sunghyun543@gmail.com</a>
-        </ContactItem>
-        <ContactItem>
-          <a href="#"> Github</a>
-        </ContactItem>
-        <ContactItem>
-          <a href="#">Portfolio</a>
-        </ContactItem>
-      </Contact> */}
     </article>
   );
 };
 
-const Contact = styled.ul`
-  display: flex;
-  justify-content: end;
-  gap: 20px;
-`;
-const ContactItem = styled.li`
-  transition: all 0.3s;
-  color: rgb(107, 114, 128);
-  &:hover {
-    cursor: pointer;
-    color: rgb(59, 130, 246);
-  }
-  a {
-    border-bottom: 1px solid rgb(107, 114, 128);
-    &:hover {
-      border-bottom: 1px solid rgb(59, 130, 246);
-    }
-  }
-`;
 const Span = styled.span`
   font-weight: 300;
   display: inline-block;
