6bf2e1ea0db8d06f194065c3ec2fd8380cb0685c
sunghyun1014 <sunghyun1014@naver.com>
2024-07-05 22:07:59 +0900

    수정: 스킬파트 display: flex -> grid 로 수정

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -1,23 +1,32 @@
 import { Article, Title } from "App";
-import { Skillbox } from "components/Projects/ProejctItem";
+
 import React from "react";
 import styled from "styled-components";
 
 export const Skill = () => {
   return (
-    <article className="my-[5rem]">
-      <Title className=" pb-2 border-b-2 border-blue-400">Skills</Title>
-      <div className="p-1 mb-3 ">
-        <h1 className="mb-3  text-[1.5rem] font-semibold ">Front-end</h1>
-        <ul className="flex justify-between text-[1rem]">
+    <div>
+      <Title>Skills</Title>
+      <div className=" mt-[1rem] ">
+        {/* <h1 className="  text-[1.5rem] font-semibold mt-[0.5rem] ">
+          Front-end
+        </h1> */}
+        <SkilGrid>
           {skillArray.map((item, index) => (
-            <li key={item + index + "1"}>
-              <Skillbox>{item}</Skillbox>
-            </li>
+            <Skillbox
+              key={item + index + "1"}
+              className={`index${index} border-[1px]`}
+            >
+              <span>{item}</span>
+            </Skillbox>
           ))}
-        </ul>
+
+          <ETCBox className="text-orange-800 font-semibold bg-[#FFE6D6] index7">
+            Git
+          </ETCBox>
+        </SkilGrid>
       </div>
-      <div className="p-1">
+      {/* <div className="px-1">
         <h1 className="mb-3  text-[1.5rem] font-semibold ">ETC</h1>
         <ul className="w-[50%] flex justify-between mt-1  text-[1rem]">
           {ETCArray.map((item, index) => (
@@ -26,13 +35,55 @@ export const Skill = () => {
             </li>
           ))}
         </ul>
-      </div>
-    </article>
+      </div> */}
+    </div>
   );
 };
+const SkilGrid = styled.ul`
+  width: 100%;
 
-const ETCBox = styled.span`
-  text-transform: uppercase;
+  display: grid;
+  grid-template-columns: repeat(9, 35px);
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  .index0 {
+    grid-column: 1/3;
+  }
+  .index1 {
+    grid-column: 3/5;
+  }
+  .index2 {
+    grid-column: 5/7;
+  }
+  .index3 {
+    grid-column: 7/-1;
+  }
+  .index4 {
+    grid-column: 1/5;
+  }
+  .index5 {
+    grid-column: 5/8;
+  }
+  .index6 {
+    grid-column: 8/-1;
+  }
+  .index7 {
+    grid-column: 1/3;
+    grid-row: 4/5;
+  }
+`;
+export const Skillbox = styled.li`
+  text-align: center;
+  display: inline-block;
+  padding: 5px 4px;
+  font-size: 0.9rem;
+  border-radius: 8px;
+  font-weight: 500;
+  background-color: #dbf2ff;
+  color: rgb(29, 78, 216);
+`;
+const ETCBox = styled.li`
   font-weight: 500;
   text-align: center;
   min-width: 50px;
@@ -41,15 +92,15 @@ const ETCBox = styled.span`
   font-size: 0.9rem;
   border-radius: 8px;
   background-color: #dbf2ff;
-  color: black;
+  color: orange;
 `;
 const skillArray = [
-  "HTML/CSS",
   "Javascript",
   "Typescript",
   "React",
   "React-query",
-  "tailwindCSS",
-  "styled-component",
+  "Styled-component",
+  "TailwindCSS",
+  "Postman",
 ];
-const ETCArray = ["git", "github", "vscode", "postman"];
+const ETCArray = ["git"];
