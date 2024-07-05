fcf36e684cc8059d8b72977eb0089bee57b72a80
sunghyun1014 <sunghyun1014@naver.com>
2024-07-05 22:07:12 +0900

    추가: 프로젝트 파트 추가

--- src/components/Projects/ProejctItem.tsx
+++ src/components/Projects/ProejctItem.tsx
@@ -1,3 +1,4 @@
+import { Skillbox } from "components/Skill/Skill";
 import React from "react";
 import styled from "styled-components";
 
@@ -18,54 +19,72 @@ type IProejctItem = {
   index: number;
 };
 export const ProejctItem = ({ item, index }: IProejctItem) => {
+  const renderText = (item: projectItemType) => {
+    switch (item.personnel.length) {
+      case 1:
+        return <li>프론트엔드 1명</li>;
+
+      default:
+        return (
+          <>
+            <li className={` border-r-2 box-border pr-[1rem] mr-[1rem]`}>{`${
+              item.personnel[0] + item.personnel[1]
+            }인 (프론트엔드 ${item.personnel[0]}명, 백엔드 ${
+              item.personnel[1]
+            }명)`}</li>
+            <li className="">{item.role}</li>
+          </>
+        );
+    }
+  };
   return (
     <div
-      className={`mb-[3.5rem]
+      className={`mb-[1.5rem]
      ${index === 0 && "mt-[1rem]"}
-     ${index !== 0 && "border-t-2 border-blue-100 pt-5"}
-      p-2`}
+     ${index !== 0 && "border-t-2 border-blue-100 pt-3"}
+      px-2`}
     >
       <div className="flex justify-between items-end">
-        <h1 className="text-3xl font-bold">{item.title}</h1>
+        <h1 className="text-[1.75rem] leading-[2rem] font-bold">
+          {item.title}
+        </h1>
         <span className="font-medium text-gray-500">{item.date}</span>
       </div>
-      <span className="pt-4 inline-block">{item.summary}</span>
+      <span className="pt-2 inline-block">{item.summary}</span>
 
-      <ul className=" border-t-[1px] mt-[1.7rem] ">
-        <li className="flex border-b-[1px] py-2">
-          <SectionChapter className="w-[30%]">
+      <ul className=" pt-[0.75rem] ">
+        <li className="flex border-t-[1px] py-[6px]">
+          <ProjectChapter>
             {item.category === "solo" ? "개인 프로젝트" : "팀 프로젝트"}
-          </SectionChapter>
-          <div className="w-[70%] flex flex-col">
-            <span>프론트엔드 담당</span>
-            {item.personnel.length === 1 ? (
-              <span className="pt-2">프론트엔드 1명</span>
-            ) : (
-              <span className="pt-2">{`프론트엔드 ${item.personnel[0]}명 / 백엔드 ${item.personnel[1]}명`}</span>
-            )}
-          </div>
+          </ProjectChapter>
+          <ProjectContent className=" items-center ">
+            {renderText(item)}
+          </ProjectContent>
         </li>
-        <li className="flex border-b-[1px] py-2 items-center">
-          <SectionChapter className="w-[30%]">활용한 기술 스택</SectionChapter>
-          <div className="gap-2 flex w-[70%]">
+        <li className="flex border-t-[1px] py-2 items-center">
+          <ProjectChapter>활용한 기술 스택</ProjectChapter>
+          <ProjectContent className="gap-2  ">
             {item.skills.map((item, index) => (
               <Skillbox key={item + index}>{item}</Skillbox>
             ))}
-          </div>
+          </ProjectContent>
         </li>
-        <li className="flex border-b-[1px] py-2">
-          <SectionChapter>프로젝트 기여</SectionChapter>
-          <ul className="w-[70%] px-1">
+        <li className="flex border-t-[1px] py-1">
+          <ProjectChapter>프로젝트 핵심기능</ProjectChapter>
+          <ProjectContent className="flex-col px-1 ">
             {item.functions.map((item, index) => (
-              <FunctionsLi key={item + index} className="py-1">
+              <FunctionsLi
+                key={item + index}
+                className="py-1 tracking-tight break-keep"
+              >
                 {item}
               </FunctionsLi>
             ))}
-          </ul>
+          </ProjectContent>
         </li>
-        <li className="flex py-2">
-          <SectionChapter>링크</SectionChapter>
-          <ul className="w-[70%] px-1">
+        <li className="flex border-t-[1px] pt-1">
+          <ProjectChapter>링크</ProjectChapter>
+          <ProjectContent className="flex-col px-1">
             {item.github && (
               <li className="pb-1">
                 <ProjectLink
@@ -88,7 +107,7 @@ export const ProejctItem = ({ item, index }: IProejctItem) => {
                 </ProjectLink>
               </li>
             )}
-          </ul>
+          </ProjectContent>
         </li>
       </ul>
     </div>
@@ -104,11 +123,15 @@ const ProjectLink = styled.a`
     color: rgb(59, 130, 246);
   }
 `;
-const SectionChapter = styled.span`
+const ProjectChapter = styled.span`
   display: inline-block;
   font-size: 1.1rem;
   font-weight: 500;
-  width: 30%;
+  width: 25%;
+`;
+const ProjectContent = styled.ul`
+  display: flex;
+  width: 75%;
 `;
 const FunctionsLi = styled.li`
   list-style: disc;
@@ -116,14 +139,4 @@ const FunctionsLi = styled.li`
   &::marker {
     color: #5ea3ec;
   }
-`;
-
-export const Skillbox = styled.span`
-  display: inline-block;
-  padding: 5px 10px;
-  font-size: 0.9rem;
-  border-radius: 8px;
-  font-weight: 500;
-  background-color: #dbf2ff;
-  color: rgb(29, 78, 216);
 `;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,7 +5,7 @@ import { ProejctItem } from "./ProejctItem";
 export const Projects = () => {
   return (
     <Article>
-      <Title className="pb-2 border-b-2 border-blue-400">Projects</Title>
+      <Title className="pb-[0.5rem]">Projects</Title>
       {ProejectArray.map((item, index) => (
         <ProejctItem
           item={item}
@@ -21,10 +21,10 @@ const ProejectArray = [
   {
     title: "MyPortfolio",
     summary:
-      "포트폴리오를 웹 형태로 제작한 프로젝트 입니다. 사용한 기술을 복습하며, 저만의 웹 포트폴리오를 만들기위해 제작하게 되었습니다",
+      "해당 프로젝트는 사용한 기술을 복습하고, 저만의 포트폴리오를 만들기 위해 웹 형태로 제작하였습니다",
     category: "solo",
     personnel: [1],
-    role: "프론트엔드 담당",
+    role: "프론트엔드 담당 (기여도 100%)",
     github: "https://github.com/sunghyun-biblee/MyPortfolio",
     deploy: "https://github.com/sunghyun-biblee/MyPortfolio",
     skills: ["React", "typescript", "tailwindCSS", "styled-component"],
@@ -41,7 +41,7 @@ const ProejectArray = [
       "온라인 학습시스템과 커뮤니티가 포함되어, 언제 어디서나 학습에 접근할 수 있는 플랫폼을 제공하기위한 서비스",
     category: "team",
     personnel: [1, 3],
-    role: "프론트엔드 담당",
+    role: "프론트엔드 담당 (기여도: 30%)",
     github: "https://github.com/sunghyun-biblee/ClassHub",
     deploy: "https://devproject.store/",
     skills: [
@@ -52,11 +52,10 @@ const ProejectArray = [
       "styled-component",
     ],
     functions: [
-      "유저관련 페이지 구현(구글 로그인,마이페이지) - 소셜로그인(Google)으로 회원가입 및 로그인시 발급받는 JWT 토큰을 쿠키로 관리, 유저정보는 ContextAPI를 사용하여 전역적 관리",
+      "유저관련 페이지 구현 - 소셜로그인(Google)으로 회원가입 및 로그인시 발급받는 JWT 토큰을 쿠키로 관리, 유저정보는 ContextAPI를 사용하여 전역적 관리",
       "사용자들의 질답 또는 스터디모집을 위해 소통을 할 수있는 커뮤니티 페이지 - React-query의 Mutation 사용하여 댓글목록 및 게시글 목록에 낙관적 업데이트 적용",
-      "강의 상세페이지 및 강의 결제 기능 구현 - PortOne(모든 결제를 지원하는 API)을 사용하여 테스트 결제 적용",
-      "강의를 추후에 구매할 수 있도록 장바구니 페이지 구현",
-      "강사 권한이 등록된 사용자가 강의등록 할 수 있는 페이지 구현",
+      "결제 기능 구현 - PortOne(모든 결제를 지원하는 API)을 사용하여 테스트 결제 적용",
+
       "학습을 위한 영상 시청 페이지 - localStorage를 사용하여, 영상을 멈추거나 종료할 시 영상의 정보를 저장하여 이어듣기 기능을 제작",
     ],
     date: "2024.05.01 ~ 2024.06.07",
@@ -64,16 +63,16 @@ const ProejectArray = [
   {
     title: "Trip_Fixie",
     summary:
-      "여행일정에 맞춰 관광지 정보와 축제정보, 숙소 정보를 제공하여 일정을 계획하고 비교하기 쉽도록 도움을 제공하는 서비스",
+      "여행일정에 맞춰 관광지 정보, 축제정보, 숙소 정보를 제공하여 일정을 계획하고 비교하기 쉽도록 도움을 제공하는 서비스",
     category: "team",
     personnel: [1, 1],
-    role: "프론트엔드 담당",
+    role: "프론트엔드 담당 (기여도: 55%)",
     github: "https://github.com/sunghyun-biblee/Trip_Fixie_Project",
     deploy: null,
     skills: ["React", "javascript", "styled-component", "framer-motion"],
     functions: [
       "여행 일정에 맞춰 관광지, 정보, 숙소 정보를 제공",
-      "제공된 장소의 상세정보와 위치정보를 TourAPI(4.0), NAVER Maps API 을 통해 제공 ",
+      "장소의 상세정보와 위치정보를 TourAPI(4.0), NAVER Maps API 을 통해 제공 ",
       "여행 일정 기간동안 OpenWeatherAPI를 통해 날씨정보 제공",
       "세워둔 여행계획을 마이리스트에 저장 후 비교 가능하도록 구현",
     ],
